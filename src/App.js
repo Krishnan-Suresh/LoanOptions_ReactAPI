8e965d46246377c9da7c3d9a4fe11876d199c0e7
Krishnan-Suresh <krishnan21au@gmail.com>
2022-06-19 17:46:47 +1000

    Adding MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import './App.css';
 import React from 'react';
 import axios from 'axios';
+import { Button } from '@material-ui/core';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   const [data, setData] = React.useState([]);
@@ -22,41 +31,87 @@ function App() {
     }
   };
 
+  const useStyles = makeStyles({
+    table: {
+      border: '4px solid #000000',
+      width: 'auto',
+      margin: 'auto',
+      marginBottom: '10px'
+    },
+    bold: {
+      fontWeight: 900
+    }
+   });
+
+   const classes = useStyles();
+
   return (
     <div>
-      <table>
-        <tr>
-          <th>Domains</th>
-          <th>Web pages</th>
-          <th>State province</th>
-          <th>Name</th>
-          <th>Country</th>
-          <th>Country code</th>
-        </tr>
-        {data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.domains.map((d, k) => {
-                return (
-                  <div>{d}</div>
-                )
-              })}</td>
-              <td>{val.web_pages.map((w, k) => {
-                return (
-                  <div>{w}</div>
-                )
-              })}</td>
-              <td>{val.state_province}</td>
-              <td>{val.name}</td>
-              <td>{val.country}</td>
-              <td>{val.alpha_two_code}</td>
-            </tr>
-          )
-        })}
-      </table>
-      <button onClick={loadData}>LOAD</button>
-      <button onClick={addData}>ADD</button>
-      <button onClick={deleteData}>DELETE</button>
+      <TableContainer component={Paper}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell align='right'><Typography className={classes.bold}>Domains</Typography></TableCell>
+              <TableCell align='right'><Typography className={classes.bold}>Web pages</Typography></TableCell>
+              <TableCell align='right'><Typography className={classes.bold}>State</Typography></TableCell>
+              <TableCell align='right'><Typography className={classes.bold}>Name</Typography></TableCell>
+              <TableCell align='right'><Typography className={classes.bold}>Country</Typography></TableCell>
+              <TableCell align='right'><Typography className={classes.bold}>Country Code</Typography></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((val, key) => {
+              return (
+                <TableRow key={key}>
+                  <TableCell align='right'>{val.domains.map((d, k) => {
+                    return (
+                      <div>{d}</div>
+                    )
+                  })}</TableCell>
+                  <TableCell align='right'>{val.web_pages.map((w, k) => {
+                    return (
+                      <div>{w}</div>
+                    )
+                  })}</TableCell>
+                  <TableCell align='right'>{val.state_province}</TableCell>
+                  <TableCell align='right'>{val.name}</TableCell>
+                  <TableCell align='right'>{val.country}</TableCell>
+                  <TableCell align='right'>{val.alpha_two_code}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Typography align='center'>
+        <Button
+          color='primary'
+          size='large'
+          type='submit'
+          variant='contained'
+          onClick={loadData}
+        >
+          LOAD
+        </Button>
+        <Button
+          color='primary'
+          size='large'
+          type='submit'
+          variant='contained'
+          onClick={addData}
+        >
+          ADD
+        </Button>
+        <Button
+          color='primary'
+          size='large'
+          type='submit'
+          variant='contained'
+          onClick={deleteData}
+        >
+          DELETE
+        </Button>
+      </Typography>
     </div>
   );
 }
